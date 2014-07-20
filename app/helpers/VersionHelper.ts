38ad92d6d9766b0bf14aceb8a160c9c1f9ced380
Adrien Cadet <acadet@live.fr>
2014-07-20 17:40:09 +0200

    Fixed issues with favicons

--- app/helpers/VersionHelper.ts
+++ app/helpers/VersionHelper.ts
@@ -5,7 +5,7 @@ class VersionHelper {
 	//region Fields
 	
 	private static _target : string = 'http://yimello.adriencadet.com/version';
-	private static _version : string = '0.1.0';
+	private static _version : string = '0.1.1';
 
 	//endregion Fields
 	
